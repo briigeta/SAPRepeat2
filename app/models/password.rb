f44ca6be8f1bfa6084d9aa04259c3c8452ad72e0
briigeta <bridgetdkabouet@gmail.com>
2023-07-27 05:18:39 +0100

    Changes to be committed: 	modified:   app/models/password.rb

--- app/models/password.rb
+++ app/models/password.rb
@@ -1,4 +1,8 @@
 class Password < ApplicationRecord
     has_secure_password
     belongs_to :user
+
+    validates :website, presence: true
+    validates :username, presence: true
+    validates :password, presence: true, length: { minimum: 6 }
 end
